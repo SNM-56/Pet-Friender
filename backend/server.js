30a1648859bf65a4e313fc3ac67404bc25c20143
Christopher Suzukida <chris.suzukida@gmail.com>
2023-03-04 10:32:18 -0800

    Migrate API fetch logic to server.js. -Include pg-pool package for backend logic
    Co-authored-by: Jamie Lee <jamieslee97@users.noreply.github.com>
    Co-authored-by: triciacorwin <triciacorwin@users.noreply.github.com>
    Co-authored-by: jjhuang3 <jjhuang3@users.noreply.github.com>
    Co-authored-by: Alex T. Klein <a-t-klein@users.noreply.github.com>
    Co-authored-by: csuzukida <47435313+csuzukida@users.noreply.github.com>"

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
 // server static files
+const idKey = 'Td80x9tGqOQnNnlwX3oKu9hjvYBqbYZnuzGwijbPd4iEmsb7EH';
+const secret = 'SdDvmwwjpY4zjKYCpmGtwqGznXQu5JxY4ro8jOfK';
+let token;
+
+const fetchAccessToken = async () => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'client_credentials');
+  params.append('client_id', idKey);
+  params.append('client_secret', secret);
+  const petFinderRes = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+    method: 'POST',
+    body: params
+  });
+  token = await petFinderRes.json();
+};
+
+fetchAccessToken();
+
+console.log('petFinder auth Token: LAST TIME ', token);
