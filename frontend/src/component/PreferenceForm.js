9c7e62a3057b2d22dc4c2efe9359394a932c74dd
Tricia Yeh <triciayeh@Tricias-MacBook-Air.local>
2023-03-04 22:54:25 -0800

    Added preference form and signup form with styling Co-authored-by: Jamie Lee <jamieslee97@users.noreply.github.com> Co-authored-by: triciacorwin <triciacorwin@users.noreply.github.com> Co-authored-by: jjhuang3 <jjhuang3@users.noreply.github.com> Co-authored-by: Alex T. Klein <a-t-klein@users.noreply.github.com> Co-authored-by: csuzukida <47435313+csuzukida@users.noreply.github.com>

---
+++ frontend/src/component/PreferenceForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function PreferenceForm() {
+  // Save submitted options
+
+  return (
+    <div className="preferenceForm">
+      <h1>Preferences</h1>
+      <div>
+        <select name="species" id="species">
+          <option value="species">Preferred Species</option>
+          <option value="Dog">Dog</option>
+          <option value="Cat">Cat</option>
+        </select>
+      </div>
+      <div>
+        <select name="age" id="age">
+          <option value="age">Preferred Age</option>
+          <option value="Young">Young</option>
+          <option value="Old">Old</option>
+        </select>
+      </div>
+      <div>
+        <select name="gender" id="gender">
+          <option value="gender">Preferred Gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </div>
+      <div>
+        <select name="size" id="size">
+          <option value="size">Preferred Size</option>
+          <option value="large">Large</option>
+          <option value="medium">Medium</option>
+          <option value="medium">Small</option>
+        </select>
+      </div>
+      <button className="submitButton" type="submit">Next</button>
+    </div>
+  );
+}
+
+export default PreferenceForm;
